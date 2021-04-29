f3b4e954d5aa0cc435c4fd63567956b519547f5c
Bhanu Prakash Vejju <bhanuprakash.vejju@genesys.com>
2021-04-29 14:23:10 +0530

    deploying

--- vue.config.js
+++ vue.config.js
@@ -4,4 +4,7 @@ module.exports = {
   configureWebpack: {
     devtool: process.env.NODE_ENV === "production" ? false : "source-map",
   },
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/hello-world/'
+  : '/'
 };
