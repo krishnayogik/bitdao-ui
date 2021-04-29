593d750f2f2ccce250a6d416702f6551d58bb28f
Bhanu Prakash Vejju <bhanuprakash.vejju@genesys.com>
2021-04-29 14:47:05 +0530

    revert deploy changes

--- vue.config.js
+++ vue.config.js
@@ -3,8 +3,5 @@
 module.exports = {
   configureWebpack: {
     devtool: process.env.NODE_ENV === "production" ? false : "source-map",
-  },
-  publicPath: process.env.NODE_ENV === 'production'
-  ? '/hello-world/'
-  : '/'
+  }
 };
